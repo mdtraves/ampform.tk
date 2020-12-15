5a9a6aaa6002818fe8dba3ce84aa60ada3e0f757
MikeTraves <mdtraves@hotmail.com>
2020-12-15 15:00:19 +0000

    post folder redirect

--- post.php
+++ post.php
@@ -3,22 +3,22 @@
     {
         
 
-        /*//-->MUST BE 'https://';*/
-        header("Content-type: application/json");
-        header("Access-Control-Allow-Credentials: true");
-        header("Access-Control-Allow-Origin: *.ampproject.org");
-        header("AMP-Access-Control-Allow-Source-Origin: ".$domain_url);
+/*//-->MUST BE 'https://';*/
+header("Content-type: application/json");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Origin: *.ampproject.org");
+header("AMP-Access-Control-Allow-Source-Origin: ".$domain_url);
+
+$redirect_url = 'ampform.tk/ty.php';
 
 
-       
-       
-        /*/--Assuming all validations are good here--*/
-        header("Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin");   
+/*/--Assuming all validations are good here--*/
+header("Access-Control-Expose-Headers: AMP-Access-Control-Allow-Source-Origin");   
 
-                echo json_encode(array('successmsg'=>$_POST['name'].'My success message. [It will be displayed shortly(!) if with redirect]'));
-                header("AMP-Redirect-To: /ty.php");
+    echo json_encode(array('successmsg'=>$_POST['name'].'My success message. [It will be displayed shortly(!) if with redirect]'));
+    header("AMP-Redirect-To: ".$redirect_url);
 
-                die();
-            }
-            
-            ?>
+    die();
+}
+
+?>
